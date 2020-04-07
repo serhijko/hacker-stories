627a266f6164225df440b8013e02bf1441bf7fb9
Serhij Korneluk <serhij@debian.home>
2020-04-07 04:22:14 +0300

    Reusable React Component

--- src/App.js
+++ src/App.js
@@ -53,7 +53,12 @@ const App = () => {
     <div>
       <h1>My Hacker Stories</h1>
 
-      <Search search={searchTerm} onSearch={handleSearch} />
+      <InputWithLabel
+        id="search"
+        label="Search"
+        search={searchTerm}
+        onInputChange={handleSearch}
+      />
 
       <hr />
 
@@ -62,14 +67,21 @@ const App = () => {
   );
 }
 
-const Search = ({ search, onSearch }) => (
+const InputWithLabel = ({
+  id,
+  label,
+  value,
+  type='text',
+  onInputChange,
+}) => (
   <>
-    <label htmlFor="search">Search: </label>
+    <label htmlFor={id}>{label}</label>
+    &nbsp;
     <input
-      id="search"
-      type="text"
-      value={search}
-      onChange={onSearch}
+      id={id}
+      type={type}
+      value={value}
+      onChange={onInputChange}
     />
   </>
 );

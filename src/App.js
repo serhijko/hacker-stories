4f9ec57b9e836c227d27eee47ffbdc0c957aad51
Serhij Korneluk <serhij@debian.home>
2020-04-07 03:49:56 +0300

    React Fragments

--- src/App.js
+++ src/App.js
@@ -63,7 +63,7 @@ const App = () => {
 }
 
 const Search = ({ search, onSearch }) => (
-  <div>
+  <>
     <label htmlFor="search">Search: </label>
     <input
       id="search"
@@ -71,7 +71,7 @@ const Search = ({ search, onSearch }) => (
       value={search}
       onChange={onSearch}
     />
-  </div>
+  </>
 );
 
 const List = ({ list }) =>
